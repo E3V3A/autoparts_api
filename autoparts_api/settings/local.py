67dfb3c2d504322215fa14ea888a48495cd35c1b
Michael Chiciak <mchicia1@gmail.com>
2018-01-18 22:06:46 -0500

    Fixed allowed_hosts.  Had append and should have been summation of two arrays.

--- autoparts_api/settings/local.py
+++ autoparts_api/settings/local.py
@@ -1,4 +1,4 @@
 from .base import *
 
 SECRET_KEY = '&%(5nw1mf-&2*o51#m0paf%flsa*v*h*&nx$nl#sd4ma^v@x=u'
-ALLOWED_HOSTS.append(["localhost", "127.0.0.1", ])
+ALLOWED_HOSTS = ALLOWED_HOSTS + ["localhost", "127.0.0.1", ]
